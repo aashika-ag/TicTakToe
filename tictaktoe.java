5d35f095b3ae0e853afcd0f3cf56bd188a7c58b1
Aashika Agarwal <aashikaagarwal31@gmail.com>
2026-04-28 09:27:31 +0530

    Accept User Slot Input (1–9)

--- tictaktoe.java
+++ tictaktoe.java
@@ -1,40 +1,16 @@
-import java.util.Random;
+import java.util.Scanner;
 
-public class tictaktoe
-{
-
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
+public class tictaktoe {
 
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
+        Scanner scanner = new Scanner(System.in);
+        int slot = getUserSlot(scanner);
+        System.out.println("Slot entered: " + slot);
+        scanner.close();
     }
 
-    static void tossAndAssignSymbols() {
-        Random random = new Random();
-        boolean toss = random.nextBoolean();
-
-        if (toss) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("Human starts first");
-        } else {
-            System.out.println("Computer starts first");
-        }
-
-        System.out.println("Human symbol: " + humanSymbol);
-        System.out.println("Computer symbol: " + computerSymbol);
+    static int getUserSlot(Scanner scanner) {
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
