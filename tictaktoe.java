4d71d1d51c85f0210e624f0dbba17aed00cabba2
Aashika Agarwal <aashikaagarwal31@gmail.com>
2026-04-28 09:14:31 +0530

    Toss to Decide First Player and Symbol

--- tictaktoe.java
+++ tictaktoe.java
@@ -1,32 +1,40 @@
-public class tictaktoe 
-{
-    public static void initializeBoard(char[][] board) 
-    {
-        for (int i = 0; i < 3; i++) 
-        {          
-            for (int j = 0; j < 3; j++) 
-            {     
-                board[i][j] = '-';
-            }
-        }
-    }
-    public static void printBoard(char[][] board) 
-    {
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) 
-        {
-            for (int j = 0; j < 3; j++) 
-            {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); 
-        }
-    }
-    public static void main(String[] args) 
-    {
-        char[][] board = new char[3][3];  
+import java.util.Random;
 
-        initializeBoard(board);  
-        printBoard(board);      
+public class tictaktoe
+{
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        boolean toss = random.nextBoolean();
+
+        if (toss) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human starts first");
+        } else {
+            System.out.println("Computer starts first");
+        }
+
+        System.out.println("Human symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
     }
 }
