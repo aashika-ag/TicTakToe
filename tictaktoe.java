52cc57b954d5301f792a00e05e882d0f72db5e56
Aashika Agarwal <aashikaagarwal31@gmail.com>
2026-04-29 10:42:59 +0530

    UC10: Detect Draw Condition

--- tictaktoe.java
+++ tictaktoe.java
@@ -1,54 +1,34 @@
 public class tictaktoe {
 
-    // UC9: Check if a player has won
-    public static boolean checkWin(char[][] board, char symbol) {
+    // UC10: Check if the game is a draw
+    public static boolean isDraw(char[][] board) {
 
-        // Check rows
+        // Traverse entire board
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                board[i][1] == symbol &&
-                board[i][2] == symbol) {
-                return true;
+            for (int j = 0; j < 3; j++) {
+
+                // If any cell is empty, it's not a draw
+                if (board[i][j] == '-') {
+                    return false;
+                }
             }
         }
 
-        // Check columns
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol &&
-                board[1][j] == symbol &&
-                board[2][j] == symbol) {
-                return true;
-            }
-        }
-
-        // Check diagonals
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol) {
-            return true;
-        }
-
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol) {
-            return true;
-        }
-
-        return false; // no win found
+        return true; // No empty cells → draw
     }
 
     // Test
     public static void main(String[] args) {
         char[][] board = {
-            {'X', 'X', 'X'},
-            {'-', 'O', '-'},
-            {'-', '-', 'O'}
+            {'X', 'O', 'X'},
+            {'X', 'O', 'O'},
+            {'O', 'X', 'X'}
         };
 
-        if (checkWin(board, 'X')) {
-            System.out.println("X Wins!");
+        if (isDraw(board)) {
+            System.out.println("It's a Draw!");
         } else {
-            System.out.println("No Winner Yet");
+            System.out.println("Game Not Finished");
         }
     }
 }
